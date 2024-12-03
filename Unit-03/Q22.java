f0e464281ab91277b7f4ea71bac797330d4ecd26
ombiswas <ombiswas345@gmail.com>
2024-12-03 15:59:31 +0530

    Started Unit-03

--- Unit-03/Q22.java
+++ Unit-03/Q22.java
@@ -1,3 +1,13 @@
-public class Q22 {
-    
+class Q22 {
+    public static void main(String args[]){
+        int a=100,b=0,c;
+        try{
+            c=a/b; //will throw exception as division by zero
+            System.out.println("Your output: "+ c);
+        } catch(ArithmeticException e){
+            System.out.println("Division by zero is not allowed.");
+        } finally {
+            System.out.println("Finally block executed.");
+        }
+    }
 }
