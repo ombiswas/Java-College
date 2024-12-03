ec60a26dede5cbdef2291e84f5dfd0b8cba9373f
ombiswas <ombiswas345@gmail.com>
2024-12-03 11:04:07 +0530

    Added Unit-02

--- Unit-02/Q21.java
+++ Unit-02/Q21.java
@@ -1,5 +1,24 @@
 class Q21 {
+    String name;
+    int age;
+
+    // Constructor with parameters using 'this' keyword
+    Q21(String name, int age) {
+        this.name = name; // 'this' refers to the instance variable
+        this.age = age;
+    }
+
+    // Method to display details
+    void display() {
+        System.out.println("Name: " + this.name);
+        System.out.println("Age: " + this.age);
+    }
+
     public static void main(String[] args) {
-        System.err.println("Hello");
+        // Create a Person object
+        Q21 person1 = new Q21("Om Biswas", 25);
+
+        // Display details
+        person1.display();
     }
 }
